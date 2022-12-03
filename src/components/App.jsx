d5ecd07a178e2e2ee82b5b86f8bf389917e08eb3
Mateus Miguel <webmateusmiguel@gmail.com>
2022-12-03 20:33:15 -0300

    add image suporto to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import sword from "../images/swc-sword.png";
+import swordSVG from "../images/sword.svg";
 
 const App = () => {
   return (
@@ -9,7 +11,10 @@ const App = () => {
         <section>
           <h1>Oh, Hai, React!</h1>
         </section>
+        <img src={sword} alt="sword" width="250" />
+        <img src={swordSVG} alt="sword" width="250" />
       </main>
+      <h1>teste</h1>
 
       <Recipes />
     </>
--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,28 @@ module.exports = {
   mode: mode,
   target: target,
 
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset", //webpack is gonna automatic determine based on a default max width (wich is 8kb according the docs) whether it should be inlined or whether it should be in the image directory
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 30 * 1024,
+        //   },
+        // },
+      },
+      {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
           "css-loader",
           "postcss-loader",
           "sass-loader",
