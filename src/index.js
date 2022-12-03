3d7523f5d7de8c4c5bd2f78b584e96c246a3bf08
Mateus Miguel <webmateusmiguel@gmail.com>
2022-12-03 14:48:06 -0300

    add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenshieldRecipe = {
   refineMoonstone: 4,
 };
 
+const elvenGauntLetsRecipe = {
+  ...elvenshieldRecipe,
+  leather: 1,
+  refineMoonstone: 2,
+};
+
 console.log(elvenshieldRecipe);
+console.log(elvenGauntLetsRecipe);
